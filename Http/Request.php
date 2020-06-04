27e998607f219d7e1cb7ef3e19065c2204a3b6d4
Eric Quesada <equesada@ludonkey.com>
2020-06-04 12:50:28 +0200

    [FEATURE] be able to access to the session from the request

--- Http/Request.php
+++ Http/Request.php
@@ -2,6 +2,7 @@
 
 namespace ludk\Http;
 
+use ludk\Http\Session;
 use ludk\Http\ParameterBag;
 
 class Request
@@ -27,6 +28,8 @@ class Request
     // $_COOKIE
     public ParameterBag $cookies;
 
+    private Session $session;
+
     public function __construct(array $query = [], array $request = [], array $server = [], array $cookies = [])
     {
         $this->query = new ParameterBag($query);
@@ -36,5 +39,22 @@ class Request
         $this->headers = new ParameterBag(getallheaders());
         $this->basePath = parse_url($server['REQUEST_URI'], PHP_URL_PATH);
         $this->method = $server['REQUEST_METHOD'];
+    }
+
+    public function getSession()
+    {
+        if (!isset($this->session)) {
+            $this->session = new Session();
+        }
+        if (!$this->session->isStarted()) {
+            $this->session->start();
+        }
+        return $this->session;
+    }
+
+    public function hasSession()
+    {
+        $localSession = new Session();
+        return $localSession->isStarted();
     }
 }
