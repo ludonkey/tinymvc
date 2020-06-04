504f5ac3983c1f9a325abfe20049eb41be6adce7
Eric Quesada <equesada@ludonkey.com>
2020-06-04 08:59:06 +0200

    [BUGFIX] missing a ;

--- Http/Session.php
+++ Http/Session.php
@@ -9,59 +9,71 @@ class Session
         session_start();
     }
 
-    public function getId(){
+    public function getId()
+    {
         return session_id();
     }
 
-    public function setId(string $id) {
+    public function setId(string $id)
+    {
         session_id($id);
     }
 
-    public function getName(){
+    public function getName()
+    {
         return session_name();
     }
 
-    public function setName(string $name){
+    public function setName(string $name)
+    {
         session_name($name);
     }
 
-    public function invalidate() {
+    public function invalidate()
+    {
         session_unset();
     }
 
-    public function has(string $name) {
-        return isset($_SESSION[$name])
+    public function has(string $name)
+    {
+        return isset($_SESSION[$name]);
     }
 
-    public function get(string $name, $default = null) {
+    public function get(string $name, $default = null)
+    {
         return $_SESSION[$name] ?? $default;
     }
 
-    public function set(string $name, $value) {
+    public function set(string $name, $value)
+    {
         $_SESSION[$name]  = $value;
     }
 
-    public function all() {
+    public function all()
+    {
         return $_SESSION;
     }
 
-    public function replace(array $attributes) {
+    public function replace(array $attributes)
+    {
         session_unset();
-        foreach($attributes as $name => $value) {
+        foreach ($attributes as $name => $value) {
             $_SESSION[$name]  = $value;
         }
     }
 
-    public function remove(string $name) {
-        unset( $_SESSION[$name]);
+    public function remove(string $name)
+    {
+        unset($_SESSION[$name]);
     }
 
-    public function clear() {
+    public function clear()
+    {
         session_unset();
     }
 
-    public function isStarted() {
+    public function isStarted()
+    {
         return session_status() != PHP_SESSION_NONE;
     }
-
 }
